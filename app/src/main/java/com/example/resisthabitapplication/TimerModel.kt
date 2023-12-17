22dbc1f158801b9c7b196cf2d86f6b14e220ae9e
Omare Faruk <ofaruk0169@gmail.com>
2023-12-17 21:59:18 +0000

    i've started the data passing process

--- app/src/main/java/com/example/resisthabitapplication/TimerModel.kt
+++ app/src/main/java/com/example/resisthabitapplication/TimerModel.kt
@@ -3,8 +3,11 @@ package com.example.resisthabitapplication
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.*
 import androidx.lifecycle.viewModelScope
+import androidx.work.Data
+import androidx.work.PeriodicWorkRequestBuilder
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import java.util.concurrent.TimeUnit
 
 //import kotlinx.coroutines.launch
 
@@ -15,10 +18,32 @@ class TimerModel: ViewModel()
     private val _currentNumber = MutableStateFlow<Int>(0)
     val currentNumber = _currentNumber.asStateFlow()
 
+    // Wrap the current number in Data
+    val workerData = Data.Builder()
+        .putInt("currentNumber", 0) // You may replace 0 with a default value or the initial value of your _currentNumber
+        .build()
+
     private val _isTimerRunning = MutableStateFlow(false)
     private var isTimerRunning: Boolean = false
     private val _jobState = MutableStateFlow<Job?>(null)
     private var job: Job? = null
+
+    fun updateCurrentNumber(newNumber: Int) {
+        _currentNumber.value = newNumber
+    }
+
+
+    val addTimeRequest =
+        PeriodicWorkRequestBuilder<TimerWorker>(1, TimeUnit.SECONDS)
+            // Additional configuration
+            .setInputData(workerData)
+            .build()
+
+    //WorkManager.getInstance().enqueue(addTimeRequest)
+
+
+
+
 
 
   /*
@@ -41,6 +66,9 @@ class TimerModel: ViewModel()
         // else the timer should continue as normal.
     }
    */
+
+    //Do the work request part 2 and 3 here.
+
 
     override fun onCleared() {
         // Cancel the coroutine when the ViewModel is cleared
--- app/src/main/java/com/example/resisthabitapplication/TimerWorker.kt
+++ app/src/main/java/com/example/resisthabitapplication/TimerWorker.kt
@@ -1,48 +1,26 @@
 package com.example.resisthabitapplication
 
 import android.content.Context
+import androidx.work.Worker
 import androidx.work.WorkerParameters
 
 
-class TimerWorker(appContext: Context, workerParams: WorkerParameters) {
-    fun doWork() {
-        //Inside doWork() I need to do the counting functionality.
-        // I may have to update isTimerRunning & job as flow
-        // Indicate whether the work finished successfully with the Result
-        // in my case, if i was able to increment the counter successfully.
 
-        //add the number 1
+class TimerWorker(context: Context, workerParams: WorkerParameters) :
+    Worker(context, workerParams) {
 
+    override fun doWork(): Result {
+        // Do the background work here
+        val currentNumber = inputData.getInt("currentNumber", 0)
 
 
 
 
+        (applicationContext as? ResistHabitApplication)?.TimerModel?.updateCurrentNumber(currentNumber)
 
 
 
-
-
-
-
-
-        /*
-        if (!isTimerRunning) {
-            isTimerRunning = true
-            job = viewModelScope.launch {
-                try {
-                    while (isActive) {
-                        _currentNumber.value = _currentNumber.value + 1
-                        delay(1000)
-
-                        return Result.success()
-                    }
-                } finally {
-                    // This block is executed when the coroutine is cancelled
-                    isTimerRunning = false
-                }
-            }
-        }
-
-         */
+        // Indicate whether the work was successful
+        return Result.success()
     }
 }
