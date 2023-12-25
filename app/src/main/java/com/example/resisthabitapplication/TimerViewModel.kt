52945d5c436d98be81ed7a247b013cb1d64f13c6
Omare Faruk <ofaruk0169@gmail.com>
2023-12-25 15:00:21 +0000

    this was a waste of time

--- app/src/main/java/com/example/resisthabitapplication/TimerViewModel.kt
+++ app/src/main/java/com/example/resisthabitapplication/TimerViewModel.kt
@@ -1,13 +1,14 @@
 package com.example.resisthabitapplication
+
 import android.app.Application
 import android.content.Context
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.*
 import androidx.work.Data
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkInfo
 import androidx.work.WorkManager
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collect
@@ -17,64 +18,98 @@ import java.util.concurrent.TimeUnit
 
 class TimerViewModel(application: Application) : AndroidViewModel(application) {
 
+    //App's context is here
     private val appContext: Context = application.applicationContext
-
-    private val _currentNumber = MutableStateFlow<Int>(0)
+    //Variables and functions related to number.
+    private val _currentNumber = MutableStateFlow<Int>(6)
     val currentNumber = _currentNumber.asStateFlow()
 
     // Wrap the current number in Data
-    private val workerData = Data.Builder()
-        .putInt(
-            "currentNumber",
-            _currentNumber.value
-        ) // You may replace 0 with a default value or the initial value of your _currentNumber
-        .build()
+    private var workerData = createWorkerData()
+
+    private fun createWorkerData(): Data {
+        return Data.Builder()
+            .putInt(
+                "currentNumber",
+                _currentNumber.value
+            )
+            .build()
+    }
+
+    private fun updateWorkerData() {
+        workerData = createWorkerData()
+    }
 
     private var isTimerRunning: Boolean = false
 
     private fun updateCurrentNumber(newNumber: Int) {
         _currentNumber.value = newNumber
     }
+   //Variables and functions related to number.
+
+
 
     suspend fun startTimer() {
         if (!isTimerRunning) {
-            isTimerRunning = true
+                isTimerRunning = true
 
-            val addTimeRequest =
-                PeriodicWorkRequestBuilder<TimerWorker>(1, TimeUnit.SECONDS)
-                    .setInputData(workerData)
-                    .build()
+            coroutineScope {
 
-            // Enqueue the work
-            WorkManager.getInstance(appContext).enqueue(addTimeRequest)
+                val addTimeRequest =
+                    PeriodicWorkRequestBuilder<TimerWorker>(
+                        1,
+                        TimeUnit.SECONDS
+                    ).setInputData(workerData)
+                        .build()
 
-            WorkManager.getInstance(appContext)
-                .getWorkInfoByIdFlow(addTimeRequest.id)
-                .flowOn(Dispatchers.Default) // Set the dispatcher for downstream operations
-                .collect { workInfo ->
-                    // Process the workInfo on the specified dispatcher
-                    // Handle the work result
-                    when (workInfo.state) {
-                        WorkInfo.State.SUCCEEDED -> {
-                            // Work completed successfully
-                            val outputData = workInfo.outputData
-                            // Process the output data if needed
-                            //use the updated number function to update the number. Doing this tomorrow.
+                // Enqueue the work
+                WorkManager.getInstance(appContext)
+                    .enqueue(addTimeRequest)
+
+/*
+                WorkManager.getInstance(appContext)
+                    .getWorkInfoByIdFlow(addTimeRequest.id)
+                    .flowOn(Dispatchers.Default) // Set the dispatcher for downstream operations
+                    .collect { workInfo ->
+                        Log.d("TimerViewModel", "Received WorkInfo: ${workInfo.state}")
+
+                        // Process the workInfo on the specified dispatcher
+                        // Handle the work result
+                        when (workInfo.state) {
+
+                            WorkInfo.State.SUCCEEDED -> {
+                                // Work completed successfully
+                                val outputData = workInfo.outputData
+                                val updatedNumber = outputData.getInt("updatedNumber", 0)
+                                Log.d("TimerViewModel", "Work succeeded. Updated Number: $updatedNumber")
+
+                                // Process the output data if needed
+                                // use the updated number function to update the number
+                                updateCurrentNumber(updatedNumber)
+                            }
+
+
+                            WorkInfo.State.ENQUEUED -> {
+                                // Work is enqueued but not yet running
+                                Log.d("TimerViewModel", "Work is enqueued but not yet running")
+                            }
+
+                            WorkInfo.State.RUNNING -> {
+                                // Work is currently running
+                                Log.d("TimerViewModel", "Work is currently running")
+                            }
+                            // WorkInfo.State.CANCELLED
+                            else -> {
+                                // Handle any unexpected states
+                            }
                         }
-
-                        WorkInfo.State.FAILED -> {
-                            // Work failed
-                            val failureReason = "fail"
-                            // Handle the failure
-                        }
-                        // Handle other states as needed
-                        // WorkInfo.State.RUNNING, WorkInfo.State.CANCELLED, etc.
-                        else -> {
-                            // Handle any unexpected states
-                        }
+                        updateWorkerData()
                     }
-                }
+
+  */
+
+            }
         }
     }
-
 }
+
--- app/src/main/java/com/example/resisthabitapplication/TimerWorker.kt
+++ app/src/main/java/com/example/resisthabitapplication/TimerWorker.kt
@@ -1,26 +1,46 @@
 package com.example.resisthabitapplication
 import android.content.Context
+import android.util.Log
+import androidx.work.CoroutineWorker
+import androidx.work.ListenableWorker
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import androidx.work.workDataOf
+import kotlinx.coroutines.delay
 
 
 class TimerWorker(
     context: Context,
     workerParams: WorkerParameters
-) : Worker(context, workerParams) {
+) : CoroutineWorker(context, workerParams) {
 
-    override fun doWork(): Result {
-        // Do the background work here
-        val currentNumber = inputData.getInt("currentNumber", 0)
+    override suspend fun doWork(): Result {
+        Log.d("TimerWorker", "doWork called")
 
-        //update number here
-        val updatedNumber = currentNumber + 1
+        try {
 
-        // Send the updated number to the ViewModel
-        val outputData= workDataOf("updatedNumber" to updatedNumber)
+            delay(5000)
+            // Your existing code for updating the number
+            val currentNumber = inputData.getInt("currentNumber", 0)
+            val updatedNumber = currentNumber + 1
+            val outputData = workDataOf("updatedNumber" to updatedNumber)
+            Log.d("TimerWorker", "Updated Number: $updatedNumber")
 
-        return Result.success(outputData)
+            Log.d("TimerWorker", "Returning Result.success(outputData)")
+
+            return Result.success(outputData)
+        } catch (e: Exception) {
+            Log.e("TimerWorker", "Error in doWork: ${e.message}")
+            return Result.failure()
+        }
     }
+
+
+
+
+
+
+
 }
+
 
