472198978ec15a38915a0f8a73e96b6cdbfa13de
Omare Faruk <ofaruk0169@gmail.com>
2023-12-29 11:12:16 +0000

    deleted empty comments

--- app/src/main/java/com/example/resisthabitapplication/TimerViewModel.kt
+++ app/src/main/java/com/example/resisthabitapplication/TimerViewModel.kt
@@ -1,19 +1,14 @@
 package com.example.resisthabitapplication
 
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.*
-import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.collect
-//import kotlinx.coroutines.launch
 
-
-class TimerViewModel: ViewModel()
+class TimerViewModel(private val savedStateHandle: SavedStateHandle): ViewModel()
 {
-
     private val _currentNumber = MutableStateFlow<Int>(0)
     val currentNumber = _currentNumber.asStateFlow()
 
